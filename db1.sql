0f2e7a7091972f745a21526acabde60f8b928107
showcoding <191141969@qq.com>
2018-11-09 10:29:05 +0800

    Update db1.sql

--- db1.sql
+++ db1.sql
@@ -30,20 +30,7 @@ CREATE TABLE `pictree` (
 -- ----------------------------
 -- Records of pictree
 -- ----------------------------
-INSERT INTO `pictree` VALUES ('16', '0', null, '沈巷变');
-INSERT INTO `pictree` VALUES ('29', '16', null, '113沈东线');
-INSERT INTO `pictree` VALUES ('30', '16', null, '135沈巷线');
-INSERT INTO `pictree` VALUES ('31', '29', null, '站房');
-INSERT INTO `pictree` VALUES ('32', '29', null, '线路');
-INSERT INTO `pictree` VALUES ('33', '31', null, 'A开闭所');
-INSERT INTO `pictree` VALUES ('34', '31', null, 'B环网柜');
-INSERT INTO `pictree` VALUES ('35', '31', null, 'C配电房');
-INSERT INTO `pictree` VALUES ('36', '32', null, '1#杆塔');
-INSERT INTO `pictree` VALUES ('37', '32', null, 'A电缆');
-INSERT INTO `pictree` VALUES ('38', '32', null, 'B架空导线');
-INSERT INTO `pictree` VALUES ('39', '16', null, 'ggg');
-INSERT INTO `pictree` VALUES ('40', '0', null, 'vvv');
-INSERT INTO `pictree` VALUES ('41', '0', null, 'ttt');
+
 
 -- ----------------------------
 -- Table structure for picurl
@@ -59,12 +46,4 @@ CREATE TABLE `picurl` (
 -- ----------------------------
 -- Records of picurl
 -- ----------------------------
-INSERT INTO `picurl` VALUES ('66', '34', '/UploadFile/2018-11-06/9d84f245-585f-4a72-bddc-4518644620fc.jpg');
-INSERT INTO `picurl` VALUES ('67', '34', '/UploadFile/2018-11-06/9e432775-da47-4c25-bec6-2fde183baf0e.jpg');
-INSERT INTO `picurl` VALUES ('68', '34', '/UploadFile/2018-11-06/81f4a5fb-d7b6-4428-9494-3a6617df7776.jpg');
-INSERT INTO `picurl` VALUES ('69', '35', '/UploadFile/2018-11-06/e509ba36-e0fc-4ec7-958e-8b025badaf59.jpg');
-INSERT INTO `picurl` VALUES ('70', '35', '/UploadFile/2018-11-06/abd5d9c0-53eb-44dc-a54c-ca399c86867a.jpg');
-INSERT INTO `picurl` VALUES ('71', '33', '/UploadFile/2018-11-06/9be17fae-f05e-49e0-908a-ea46696c787c.jpg');
-INSERT INTO `picurl` VALUES ('72', '33', '/UploadFile/2018-11-06/cda3e1ab-3393-447a-bea6-9977b266df7a.jpg');
-INSERT INTO `picurl` VALUES ('73', '31', '/UploadFile/2018-11-06/ac784b33-f1de-4f76-8618-0ac3b7f8d162.jpg');
-INSERT INTO `picurl` VALUES ('82', '16', '/UploadFile/2018-11-06/1064ef97-1a89-4756-9dd8-2a645fbc73a5.jpg');
+
